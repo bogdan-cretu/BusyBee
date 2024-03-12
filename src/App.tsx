9370110ebeaffe5470d77037ca76c2f1de9a018c
bogdan-cretu <cretu.bogdan@yahoo.ro>
2024-03-12 22:40:20 +0200

    Initial commit

---
+++ src/App.tsx
@@ -0,0 +1,138 @@
+
+import { Tab, Tabs, TabList, TabPanel } from 'react-tabs';
+
+import { useState } from "react";
+import Gestiune from "./components/CalendarGestiune";
+import Soferi from "./components/CalendarSoferi";
+import Coletare from './components/CalendarColetare';
+import Strans from './components/CalendarStrans';
+import Details from "./components/Details";
+import Tiparire from './components/CalendarTiparire';
+import 'react-tabs/style/react-tabs.css';
+
+
+
+function App() {
+const [showDetails, setShowDetails] = useState(false);
+const [data, setData] = useState(null);
+
+const showDetailsHandle = (dayStr) => {
+  setData(dayStr);
+  setShowDetails(true);
+};
+
+
+  
+
+  return (<div>
+    <div className="App">
+
+    <Tabs>
+    <TabList>
+      <Tab>Gestiune</Tab>
+      <Tab>Coletare</Tab>
+      <Tab>Strans</Tab>
+      <Tab>Tiparire</Tab>
+      <Tab>Soferi</Tab>
+      <Tab>Toate echipele</Tab>
+    </TabList>
+
+    <TabPanel>
+    <div className='calendar-wrapper'>
+      <h1>Gestiune</h1>
+      <Gestiune showDetailsHandle={showDetailsHandle} />
+      <br />
+      {showDetails && <Details data={data} />}
+      </div>
+    </TabPanel>
+    <TabPanel>
+    <div className='calendar-wrapper'>
+      <h1>Coletare</h1>
+      <Coletare showDetailsHandle={showDetailsHandle} />
+      <br />
+      {showDetails && <Details data={data} />}
+      </div>
+    </TabPanel>
+    <TabPanel>
+    <div className='calendar-wrapper'>
+      <h1>Strans</h1>
+      <Strans showDetailsHandle={showDetailsHandle} />
+      <br />
+      {showDetails && <Details data={data} />}
+      </div>
+      
+    </TabPanel>
+    <TabPanel>
+    <div className='calendar-wrapper'>
+      <h1>Tiparire</h1>
+      <Tiparire showDetailsHandle={showDetailsHandle} />
+      <br />
+      {showDetails && <Details data={data} />}
+      </div>
+    </TabPanel>
+    <TabPanel>
+    <div className='calendar-wrapper'>
+      <h1>Soferi</h1>
+      <Soferi showDetailsHandle={showDetailsHandle} />
+      <br />
+      {showDetails && <Details data={data} />}
+      </div>
+    </TabPanel>
+
+
+
+
+    <TabPanel>
+    <div className='calendar-wrapper'>
+      <h1>Gestiune</h1>
+      <Gestiune showDetailsHandle={showDetailsHandle} />
+      <br />
+      {showDetails && <Details data={data} />}
+      </div>
+      <div className='calendar-wrapper'>
+      <h1>Soferi</h1>
+      <Soferi showDetailsHandle={showDetailsHandle} />
+      <br />
+      {showDetails && <Details data={data} />}
+      </div>
+      <div className='calendar-wrapper'>
+      <h1>Coletare</h1>
+      <Coletare showDetailsHandle={showDetailsHandle} />
+      <br />
+      {showDetails && <Details data={data} />}
+      </div>
+      <div className='calendar-wrapper'>
+      <h1>Strans</h1>
+      <Strans showDetailsHandle={showDetailsHandle} />
+      <br />
+      {showDetails && <Details data={data} />}
+      </div>
+      <div className='calendar-wrapper'>
+      <h1>Tiparire</h1>
+      <Tiparire showDetailsHandle={showDetailsHandle} />
+      <br />
+      {showDetails && <Details data={data} />}
+      </div>
+      <div className='calendar-wrapper'>
+      <h1>Tiparire</h1>
+      <Tiparire showDetailsHandle={showDetailsHandle} />
+      <br />
+      {showDetails && <Details data={data} />}
+      </div>
+    </TabPanel>
+  </Tabs>
+
+      
+      
+      
+      
+      
+    </div>
+
+    
+  </div>
+  
+  )
+}
+
+export default App
---
+++ src/components/Gestiune.tsx
@@ -0,0 +1,101 @@
+import { createClient } from "@sanity/client";
+
+
+async function getGestiune(){
+    const client = createClient({
+        projectId: 'nmvafbin',
+        dataset: 'production',
+        apiVersion: "2024-03-04",
+        useCdn: true,
+    });
+
+    return client.fetch(
+        `*[_type == "gestiune"]`
+    )
+}
+  const gestiune = await getGestiune();
+
+  
+
+let newList = []
+
+let week = [[],[],[],[],[]]
+
+for (let i=0; i<gestiune.length; i++){
+    for (let j=0; j<4; j++){
+        newList.push(gestiune[i])
+    }
+};
+
+
+// for (let i=0; i<gestiune.length; i++){
+//         week[0].push(gestiune[i])
+//     };
+
+
+for (let i = 0; i<newList.length; i++){
+    week[0].push(newList[i])
+    newList.shift()
+    week[1].push(newList[i])
+    newList.shift()
+    week[2].push(newList[i])
+    newList.shift()
+    week[3].push(newList[i])
+    newList.shift()
+    week[4].push(newList[i])  
+
+}
+
+week[0] = week[0].filter(function( element ) {
+    return element !== undefined;
+ });
+ week[1] = week[1].filter(function( element ) {
+    return element !== undefined;
+ });
+ week[2] = week[2].filter(function( element ) {
+    return element !== undefined;
+ });
+ week[3] = week[3].filter(function( element ) {
+    return element !== undefined;
+ });
+ week[4] =  week[4].filter(function( element ) {
+    return element !== undefined;
+ });
+
+
+
+export const Mon = () =>{
+    const mon = week[0].map((elem)=>{
+        return <span key={elem._id} className="element" >{elem.nume}</span>
+    })
+    return  <div className="day-wrapper">{mon}</div>
+    }
+
+export const Tue = () =>{
+    const tue = week[1].map((elem)=>{
+        return <span key={elem._id} className="element" >{elem.nume}</span>
+    })
+    return  <div className="day-wrapper">{tue}</div>
+    }
+
+export const Wed = () =>{
+    const wed = week[2].map((elem)=>{
+        return <span key={elem._id} className="element" >{elem.nume}</span>
+    })
+    return  <div className="day-wrapper">{wed}</div>
+    }
+
+export const Thu = () =>{
+    const thu = week[3].map((elem)=>{
+        return <span key={elem._id} className="element" >{elem.nume}</span>
+    })
+    return  <div className="day-wrapper">{thu}</div>
+    }
+
+export const Fri = () =>{
+    const fri =  week[4].map((elem)=>{
+        return <span key={elem._id} className="element" >{elem.nume}</span>
+    })
+    return  <div className="day-wrapper">{fri}</div>
+    }
+
