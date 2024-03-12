ec9dc9845ec90eb368698bc9435e6595f5682f7c
bogdan-cretu <cretu.bogdan@yahoo.ro>
2024-03-12 23:38:18 +0200

    Restructure

---
+++ src/components/CalendarGestiune.jsx
@@ -0,0 +1,164 @@
+import { useState } from "react";
+import {
+  format,
+  startOfWeek,
+  addDays,
+  isSameDay,
+  lastDayOfWeek,
+  getWeek,
+  addWeeks,
+  subWeeks
+} from "date-fns";
+import {Mon, Tue, Wed, Thu, Fri} from './Gestiune'
+
+
+
+let currentList = [<Mon />, <Tue />,<Wed />, <Thu />, <Fri /> ]
+
+Array.prototype.move = function (from, to) {
+    this.splice(to, 0, this.splice(from, 1)[0]);
+  };
+
+const calendarMonth = new Date()
+const calendarWeek = getWeek(calendarMonth)
+
+for (let i = 0; i < calendarWeek; i++){
+  currentList.move(4,0)
+  
+}
+
+const CalendarGestiune = ({ showDetailsHandle }) => {
+  const [currentMonth, setCurrentMonth] = useState(new Date());
+  const [currentWeek, setCurrentWeek] = useState(getWeek(currentMonth));
+  const [selectedDate, setSelectedDate] = useState(new Date());
+ 
+
+
+
+  const changeWeekHandle = (btnType) => {
+    if (btnType === "prev") {
+      setCurrentMonth(subWeeks(currentMonth, 1));
+      setCurrentWeek(getWeek(subWeeks(currentMonth, 1)));
+      currentList.move(0,4)
+    }
+    if (btnType === "next") {
+      setCurrentMonth(addWeeks(currentMonth, 1));
+      setCurrentWeek(getWeek(addWeeks(currentMonth, 1)));
+      currentList.move(4,0)
+    }
+  };
+
+  const onDateClickHandle = (day, dayStr) => {
+    setSelectedDate(day);
+    showDetailsHandle(dayStr);
+  };
+
+  const renderHeader = () => {
+    const dateFormat = "MMM yyyy";
+    return (
+      <div className="header row flex-middle">
+        <div className="col col-start">
+       
+        </div>
+        <div className="col col-center">
+          <span>{format(currentMonth, dateFormat)}</span>
+        </div>
+        <div className="col col-end">
+          
+        </div>
+      </div>
+    );
+  };
+  const renderDays = () => {
+    const dateFormat = "EEE";
+    const days = [];
+    let startDate = startOfWeek(currentMonth, { weekStartsOn: 1 });
+    for (let i = 0; i < 7; i++) {
+        
+      days.push(
+        <div className="col col-center" key={i}>
+          {format(addDays(startDate, i), dateFormat)}
+        </div>
+      );
+    }
+    return <div className="days row">{days}</div>;
+  };
+
+
+ 
+  const renderCells = () => {
+    const startDate = startOfWeek(currentMonth, { weekStartsOn: 1 });
+    const endDate = lastDayOfWeek(currentMonth, { weekStartsOn: 1 });
+    const dateFormat = "d";
+    const rows = [];
+    let days = [];
+    let day = startDate;
+    let formattedDate = "";
+    while (day <= endDate) {
+      for (let i = 0; i < 7; i++) {
+        formattedDate = format(day, dateFormat);
+        const cloneDay = day;
+        
+        days.push(
+          <div
+            className={`col cell ${
+              isSameDay(day, new Date())
+                ? "today"
+                : isSameDay(day, selectedDate)
+                ? "selected"
+                : ""
+            }`}
+            key={day}
+            onClick={() => {
+              const dayStr = format(cloneDay, "ccc dd MMM yy");
+              onDateClickHandle(cloneDay, dayStr);
+            }}   
+          >
+            <span className="number">{formattedDate}</span>
+            <span className="bg">{formattedDate}</span>
+            <div className="separator"></div>
+            <div>{currentList[i]}</div>
+          </div>
+        );
+        day = addDays(day, 1);
+        
+      }
+
+      rows.push(
+        <div className="row" key={day}>
+          {days}
+        </div>
+      );
+      days = [];
+    }
+    return <div className="body">{rows}</div>;
+  };
+  
+  const renderFooter = () => {
+    return (
+      <div className="header row flex-middle">
+        <div className="col col-start">
+          <div className="icon" onClick={() => changeWeekHandle("prev")}>
+            Saptamana anterioara
+          </div>
+        </div>
+        <div>Saptamana {currentWeek}</div>
+        <div className="col col-end" onClick={() => changeWeekHandle("next")}>
+          <div className="icon">Saptamana urmatoare</div>
+          
+        </div>
+      </div>
+    );
+  };
+  return (
+    <div className="calendar">
+      {renderHeader()}
+      {renderDays()}
+      {renderCells()}
+      {renderFooter()}
+    </div>
+  );
+};
+
+export default CalendarGestiune;
+
